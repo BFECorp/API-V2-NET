dcdc59c0de11e827948283b7306c5d9c7bc6e39d
Wu Qing <qing.wu@chukou1.com>
2013-12-04 10:20:19 +0800

    Updated: 更新直发下单接口调用示例代码

--- Aspnet/ExpressAddOrder.aspx.cs
+++ Aspnet/ExpressAddOrder.aspx.cs
@@ -18,8 +18,6 @@ namespace Chukou1Demo.Aspnet
 
         private void Initialize()
         {
-            txtMessageID.Text = "";
-            txtVersion.Text = "";
             pnlResponse.Visible = false;
             pnlRequest.Visible = true;
         }
@@ -29,7 +27,7 @@ namespace Chukou1Demo.Aspnet
             #region 生成包裹
 
             var product = new List<ExpressProduct>();
-            product.Add(new ExpressProduct()
+            product.Add(new ExpressProduct
             {
                 CustomsTitleCN = "怀表",
                 CustomsTitleEN = "POCKET WATCH",
@@ -41,10 +39,10 @@ namespace Chukou1Demo.Aspnet
             });
 
             var packageList = new List<ExpressPackage>();
-            packageList.Add(new ExpressPackage()
+            packageList.Add(new ExpressPackage
             {
                 Custom = "OR201112230076",
-                ShipToAddress = new ShipToAddress()
+                ShipToAddress = new ShipToAddress
                 {
                     Contact= "nicolas courtais",
                     Street1= "37 rue general ferrié",
@@ -53,8 +51,7 @@ namespace Chukou1Demo.Aspnet
                     Country = "France",
                     PostCode = "38100",
                 },
-                //ItemSign = "test123",
-                Packing = new Packing()
+                Packing = new Packing
                 {
                     Height = 1,
                     Length = 1,
@@ -70,9 +67,7 @@ namespace Chukou1Demo.Aspnet
             var orderDetail = new ExpressOrder
             {
                 IsTracking = false,
-                Location = txtLocation.Text,
-                OrderSign = txtOrderSign.Text,
-                PickupType = ddlPickUpType.SelectedIndex,
+                Location = "GZ",
                 Remark = "测试订单",
                 PackageList = packageList.ToArray(),
                 PickUpAddress = new PickUpAddress
@@ -86,7 +81,6 @@ namespace Chukou1Demo.Aspnet
                     City = "Alvin",
                     Company = "ABC",
                 },
-                //ExpressType = ExpressType.HKP,
 
             };
 
@@ -96,60 +90,13 @@ namespace Chukou1Demo.Aspnet
             UserKeyAndToken ukt = new UserKeyAndToken();
             var request = new ExpressAddOrderNewRequest
             {
-                MessageID = txtMessageID.Text,
                 UserKey = ukt.getUserKey(),
                 Token = ukt.getToken(),
                 Submit = false,
-                Version = txtVersion.Text,
                 OrderDetail = orderDetail,
                 ExpressTypeNew = "HTM",
+                MessageID = "SampleCallId"
             };
-           
-            //switch (ddlExpressType.SelectedIndex)
-            //{
-            //    case 0:
-            //        request.OrderDetail.ExpressType = ExpressType.CDD;
-            //        break;
-            //    case 1:
-            //        request.OrderDetail.ExpressType = ExpressType.CEE;
-            //        break;
-            //    case 2:
-            //        request.OrderDetail.ExpressType = ExpressType.CES;
-            //        break;
-            //    case 3:
-            //        request.OrderDetail.ExpressType = ExpressType.CNP;
-            //        break;
-            //    case 4:
-            //        request.OrderDetail.ExpressType = ExpressType.CTU;
-            //        break;
-            //    case 5:
-            //        request.OrderDetail.ExpressType = ExpressType.CUE;
-            //        break;
-            //    case 6:
-            //        request.OrderDetail.ExpressType = ExpressType.EMD;
-            //        break;
-            //    case 7:
-            //        request.OrderDetail.ExpressType = ExpressType.EMS;
-            //        break;
-            //    case 8:
-            //        request.OrderDetail.ExpressType = ExpressType.EUB;
-            //        break;
-            //    case 9:
-            //        request.OrderDetail.ExpressType = ExpressType.HKD;
-            //        break;
-            //    case 10:
-            //        request.OrderDetail.ExpressType = ExpressType.HKP;
-            //        break;
-            //    case 11:
-            //        request.OrderDetail.ExpressType = ExpressType.SGP;
-            //        break;
-            //    case 12:
-            //        request.OrderDetail.ExpressType = ExpressType.SHD;
-            //        break;
-            //    default:
-            //        request.OrderDetail.ExpressType = ExpressType.UPS;
-            //        break;
-            //}
            
             ExpressAddOrderResponse response = clint.ExpressAddOrderNew(request);
 
